3d9306e36a7f326d010d64aee6748519c5eaa159
aigt <ai-kr@yandex.ru>
2023-06-17 20:24:44 +0500

    Update client permissions

--- common/src/commonMain/kotlin/models/account/AccountPermissionClient.kt
+++ common/src/commonMain/kotlin/models/account/AccountPermissionClient.kt
@@ -3,8 +3,6 @@ package aigt.finaccounts.common.models.account
 enum class AccountPermissionClient {
     READ,
     UPDATE,
-    DELETE,
-    MAKE_VISIBLE_PUBLIC,
-    MAKE_VISIBLE_GROUP,
-    MAKE_VISIBLE_OWNER,
+    HISTORY,
+    TRANSACT,
 }
