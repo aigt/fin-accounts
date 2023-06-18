189c6b2012d876ffe7a906f76cb3e5100474031c
aigt <ai-kr@yandex.ru>
2023-06-18 19:21:35 +0500

    Add kmp mappers

--- mappers-v1-kmp/src/commonMain/kotlin/exceptions/UnknownMkplCommand.kt
+++
@@ -1,5 +0,0 @@
-package aigt.finaccounts.mappers.v1.exceptions
-
-import aigt.finaccounts.common.models.command.ContextCommand
-
-class UnknownContextCommand(command: ContextCommand) : Throwable("Wrong command $command at mapping toTransport stage")
